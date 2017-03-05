94f00a4bb155664da5bfd47b1988095d7c9ddef3
pkarira <pulkit5208@gmail.com>
2017-03-06 01:16:28 +0530

    calling functions directly from button listeners and removed if else block from function.js

--- scripts/buttonlisteners.js
+++ scripts/buttonlisteners.js
@@ -1,27 +1,19 @@
   document.addEventListener('DOMContentLoaded', function() {
   var save_now = document.getElementById('save_now');
   save_now.addEventListener('click', function() {
-    chrome.tabs.executeScript(null, {code: "var save_now=1;"}, function() {
-      chrome.tabs.executeScript(null, {file: "scripts/functions.js"}, function() {
-      });
-    });
+    save_now_function();
   }, false);
 
   var recent_capture = document.getElementById('recent_capture');
   recent_capture.addEventListener('click', function() {
-    chrome.tabs.executeScript(null, {code: "var recent_capture=1;"}, function() {
-      chrome.tabs.executeScript(null, {file: "scripts/functions.js"}, function() {
-      });
-    });
+    recent_capture_function()
   }, false);
 
   var first_capture = document.getElementById('first_capture');
   first_capture.addEventListener('click', function() {
-    chrome.tabs.executeScript(null, {code: "var first_capture=1;"}, function() {
-      chrome.tabs.executeScript(null, {file: "scripts/functions.js"}, function() {
-      });
-    });
+    first_capture_function()
   }, false);
+
   var view_all = document.getElementById('view_all');
   view_all.addEventListener('click', function() {
     chrome.tabs.executeScript(null, {code: "var view_all=1;"}, function() {
--- scripts/functions.js
+++ scripts/functions.js
@@ -31,29 +31,4 @@ function view_all_function() {
   open_url = "https://web.archive.org/web/*/"+encodeURI(url);
   document.location.href = open_url;
 }
-
-try {
-  if (save_now !== undefined && save_now==1) {
-    save_now_function();
-  }
-} catch (err) {}
-
-try {
-  if (recent_capture !== undefined && recent_capture==1) {
-    recent_capture_function();
-  }
-} catch (err) {}
-
-try {
-  if (first_capture !== undefined && first_capture==1) {
-    first_capture_function();
-  }
-} catch (err) {}
-
-try {
-  if (view_all !== undefined && view_all == 1) {
-    view_all_function();
-  }
-} catch (err) {}
-
 // chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {});
