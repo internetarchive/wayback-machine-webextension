a0f51581ab71778d3f1114c940ca8bb96f8c8472
Gurek Singh <gurek15033@iiitd.ac.in>
2017-03-13 21:55:38 +0530

    added support for Alexa Ranking and Whois
    Signed-off-by: Gurek Singh <gurek15033@iiitd.ac.in>

--- scripts/popup.js
+++ scripts/popup.js
@@ -16,6 +16,22 @@ function first_capture_function(){
 	});
 }
 
+function alexa_statistics_function(){
+	var alexa_url = "http://www.alexa.com/siteinfo/";
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+		var tab_url = tabs[0].url;
+		chrome.tabs.create({ url: alexa_url+tab_url });
+	});
+}
+
+function whois_statistics_function(){
+	var whois_url = "http://www.whois.com/whois/";
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+		var tab_url = tabs[0].url;
+		chrome.tabs.create({ url: whois_url+tab_url });
+	});
+}
+
 function view_all_function(){
 	var pattern = /https:\/\/web\.archive\.org\/web\/(.+?)\//g;
 	url = document.location.href.replace(pattern, "");
@@ -23,7 +39,8 @@ function view_all_function(){
 	document.location.href = open_url;
 }
 
-
 document.getElementById('save_now').onclick = save_now_function;
 document.getElementById('recent_capture').onclick = recent_capture_function;
 document.getElementById('first_capture').onclick = first_capture_function;
+document.getElementById('alexa_statistics').onclick = alexa_statistics_function;
+document.getElementById('whois_statistics').onclick = whois_statistics_function;
