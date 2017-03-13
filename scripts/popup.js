5100d965d238178e71a42b1884a301f672db7c8a
Gurek Singh <gurek15033@iiitd.ac.in>
2017-03-13 21:55:38 +0530

    integrated Wayback Machine's site search
    Signed-off-by: Gurek Singh <gurek15033@iiitd.ac.in>

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,9 @@
+function browse_history_function(){
+	var wb_url = "https://web.archive.org/web/*/";
+	var url_toSearch = document.getElementById('browse_history_input').value;
+	chrome.tabs.create({ url: wb_url+url_toSearch });
+}
+
 function save_now_function(){
 	var wb_url = "https://web.archive.org/save/";
 	chrome.runtime.sendMessage({message: "openurl", wayback_url: wb_url, method:'save' }, function(response) {
@@ -24,6 +30,7 @@ function view_all_function(){
 }
 
 
+document.getElementById('browse_history').onclick = browse_history_function;
 document.getElementById('save_now').onclick = save_now_function;
 document.getElementById('recent_capture').onclick = recent_capture_function;
 document.getElementById('first_capture').onclick = first_capture_function;
