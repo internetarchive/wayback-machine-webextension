d0f46b6f0f054284eb633d039a5870d36700cf60
anishsarangi <anish.kumar.sarangi@gmail.com>
2017-03-29 03:53:29 +0530

    Some Modification has been done

--- scripts/popup.js
+++ scripts/popup.js
@@ -21,6 +21,18 @@ function view_all_function(){
     open_url = "https://web.archive.org/web/*/"+encodeURI(url);
     document.location.href = open_url;
 }
+function fb_share(searchurl){    
+    var fburl="https://www.facebook.com/sharer/sharer.php?u=";
+    chrome.tabs.create({url:fburl+searchurl});
+}
+function twitter_share(searchurl){    
+    var tweeturl="https://twitter.com/intent/tweet?text=";
+    chrome.tabs.create({url:tweeturl+searchurl});
+}
+function google_share(searchurl){    
+    var googleurl="https://plus.google.com/share?url=";
+    chrome.tabs.create({url:googleurl+searchurl});
+}
 window.onload=function start()
 {
     document.getElementById("form").addEventListener("keydown",function(e)
@@ -28,53 +40,51 @@ window.onload=function start()
         if(e.keyCode===13)
         {
             var http=new XMLHttpRequest();                                          
-            var takeurl=document.getElementById("form").value;
-            var newurl="http://archive.org/wayback/available?url="+takeurl;
-            http.open("GET",newurl,true);
+            var take_url=document.getElementById("form").value;
+            var new_url="http://archive.org/wayback/available?url="+take_url;
+            http.open("GET",new_url,true);
             http.send(null);
             http.onload=function()
             {
                 var data=JSON.parse(http.response);
                 if(typeof data.archived_snapshots.closest !=="undefined")
                 {
-                    var date=data.archived_snapshots.closest.timestamp;
-                    var tosearch=data.archived_snapshots.closest.url;
-                    var exact=date.substring(0,4)+'/'+date.substring(4,6)+'/'+date.substring(6,8);
-                    var time=date.substring(8,10)+'/'+date.substring(10,12)+'/'+date.substring(12,14);
-                    var target = document.getElementById("search-option");
-                    var newElement = document.createElement('tr');
-                    newElement.setAttribute("class","spaceUnder");
-                    var newtd=document.createElement('td');
-                    newtd.setAttribute("id","new_td");
-                    newtd.style.padding='10px';
+                    var timestamp_date=data.archived_snapshots.closest.timestamp;
+                    var search_url=data.archived_snapshots.closest.url;
+                    var formatted_date=timestamp_date.substring(0,4)+'/'+timestamp_date.substring(4,6)+'/'+timestamp_date.substring(6,8);
+                    var time=timestamp_date.substring(8,10)+'/'+timestamp_date.substring(10,12)+'/'+timestamp_date.substring(12,14);
+                    var target_td = document.getElementById('search_td')
+                    target_td.innerHTML = "";
                     var new_button = document.createElement("BUTTON");
                     new_button.setAttribute("class","btn btn-success button");
-                    var t = document.createTextNode("See page On:"+exact);
-                    new_button.appendChild(t);
-                    target.parentNode.insertBefore(newElement,target.nextSibling);
-                    newElement.appendChild(newtd);
-                    newtd.appendChild(new_button);
-                    document.getElementsByClassName("button")[0].addEventListener("click",function()
+                    new_button.appendChild(document.createTextNode("See page On:"+formatted_date));
+                    target_td.appendChild(new_button);
+
+                    new_button.addEventListener("click",function()
                     {
-                        chrome.tabs.create({url:tosearch});
+                        chrome.tabs.create({url:search_url});
                     });
-                    window.searchurl=tosearch;
+                    document.getElementById('facebook_btn').addEventListener("click",function()
+                    {
+                        fb_share(search_url);
+                    });                    
+                    document.getElementById('twitter_btn').addEventListener("click",function()
+                    {
+                        twitter_share(search_url);
+                    });              
+                    document.getElementById('googleplus_btn').addEventListener("click",function()
+                    {
+                        google_share(search_url);
+                    });
                 }
                 else
                 {
-                    var target = document.getElementById("search-option");
-                    var newElement = document.createElement('tr');
-                    newElement.setAttribute("class","spaceUnder");
-                    var newtd=document.createElement('td');
-                    newtd.setAttribute("id","new_td");
-                    newtd.style.padding='0px 0px 14px 33px';
+                    var target_td = document.getElementById('search_td')
+                    target_td.innerHTML = "";
                     var new_button = document.createElement("BUTTON");
                     new_button.setAttribute("class","btn btn-danger button");
-                    var t = document.createTextNode("Page Not Found");
-                    new_button.appendChild(t);
-                    target.parentNode.insertBefore(newElement,target.nextSibling);
-                    newElement.appendChild(newtd);
-                    newtd.appendChild(new_button);
+                    new_button.appendChild(document.createTextNode("Page Not Found"));
+                    target_td.appendChild(new_button);
                 }
             };
         }
@@ -82,20 +92,4 @@ window.onload=function start()
     document.getElementById('save_now').onclick = save_now_function;
     document.getElementById('recent_capture').onclick = recent_capture_function;
     document.getElementById('first_capture').onclick = first_capture_function;
-    document.getElementById('fb-share-btn').onclick = fb_share;
-    document.getElementById('tweet-btn').onclick = tweet;
-    document.getElementById('googleplus-btn').onclick=googleshare;
-}
-var searchurl;
-function fb_share(){    
-    var fburl="https://www.facebook.com/sharer/sharer.php?u=";
-    chrome.tabs.create({url:fburl+searchurl});
-}
-function tweet(){    
-    var tweeturl="https://twitter.com/intent/tweet?text=";
-    chrome.tabs.create({url:tweeturl+searchurl});
-}
-function googleshare(){    
-    var googleurl="https://plus.google.com/share?url=";
-    chrome.tabs.create({url:googleurl+searchurl});
 }
