6d1eb84d8117b611379d35c1483417d411b45e82
Abhishek <abhidas17695@gmail.com>
2017-11-03 00:30:14 +0530

    Using Python CDX

--- scripts/background.js
+++ scripts/background.js
@@ -246,7 +246,7 @@ function checkIt(wayback_url) {
 * License: AGPL-3
 * Copyright 2016, Internet Archive
 */
-var VERSION = "2.7";
+var VERSION = "2.8";
 Globalstatuscode="";
 var excluded_urls = [
   
