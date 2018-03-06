915fcfbdff6cb2ea60ce696a2819e8f671247b1c
Rohit Kumar <rohittourister@gmail.com>
2018-03-06 16:48:54 +0530

    Whois API for Chrome extension
    Host it yourself on your server

--- host/whois.php
+++ host/whois.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /*
 * Credits
@@ -20,29 +19,5 @@ $valc = str_replace("/https","https",str_replace("eimg","https://www.whois.com/e
 $array['admin_contact'] = $valc;  
 $vald = str_replace("/https","https",str_replace("eimg","https://www.whois.com/eimg",$html->find('.df-block', 3)->innertext));
 $array['technical_contact'] = $vald;  
-echo json_encode($array, JSON_PRETTY_PRINT);
-=======
-<?php
-/*
-* Credits
-* @rohitcoder - Rohit Kumar
-* Website - https://rohitcoder.cf
-* Profile - https://github.com/rohitcoder
-*/
-header("Content-Type: application/json");
-include('simple_html_dom.php');
-$link = $_GET['site'];	   
-$url =  'https://www.whois.com/whois/'.$link;
-$html = file_get_html($url);
-$array = array(); 
-$vala = str_replace("/https","https",str_replace("eimg","https://www.whois.com/eimg",$html->find('.df-block', 0)->innertext));
-$array['domain_info'] = $vala; 
-$valb = str_replace("/https","https",str_replace("eimg","https://www.whois.com/eimg",$html->find('.df-block', 1)->innertext));
-$array['registrant_contact'] = $valb; 
-$valc = str_replace("/https","https",str_replace("eimg","https://www.whois.com/eimg",$html->find('.df-block', 2)->innertext));
-$array['admin_contact'] = $valc;  
-$vald = str_replace("/https","https",str_replace("eimg","https://www.whois.com/eimg",$html->find('.df-block', 3)->innertext));
-$array['technical_contact'] = $vald;  
-echo json_encode($array, JSON_PRETTY_PRINT);
->>>>>>> e0111953cbb1a66c7aab8a94c6abeeb7933e9dc1
+echo json_encode($array, JSON_PRETTY_PRINT); 
 ?>
