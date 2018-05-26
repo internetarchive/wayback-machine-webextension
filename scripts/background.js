f8ef793391427e9e2a235cc097404cb373c719a9
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2018-05-27 01:30:13 +0530

    Upgraded to 2.15.3

--- scripts/background.js
+++ scripts/background.js
@@ -240,7 +240,7 @@ function checkIt(wayback_url) {
 * License: AGPL-3
 * Copyright 2016, Internet Archive
 */
-var VERSION = "2.15.2";
+var VERSION = "2.15.3";
 Globalstatuscode="";
 var excluded_urls = [
   "localhost",
