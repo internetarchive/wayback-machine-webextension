c8f492e5d52cd35ce7e7f92234d77099911141b2
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2018-06-13 22:02:33 +0530

    Added boundry condition for auto-update-context

--- scripts/background.js
+++ scripts/background.js
@@ -240,7 +240,7 @@ function checkIt(wayback_url) {
 * License: AGPL-3
 * Copyright 2016, Internet Archive
 */
-var VERSION = "2.17.1";
+var VERSION = "2.17.2";
 Globalstatuscode="";
 var excluded_urls = [
   "localhost",
@@ -546,15 +546,49 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
                 }
               }else{
                 if(event.show_context=="tab"){
-                  var alexa_url="http://www.alexa.com/siteinfo/" + url;
-                  chrome.tabs.update(parseInt(tabId1), {url:alexa_url});
-                  var whois_url="https://www.whois.com/whois/" + url;
-                  chrome.tabs.update(parseInt(tabId2), {url:whois_url});
-                  var tweet_url="https://twitter.com/search?q="+open_url;
-                  chrome.tabs.update(parseInt(tabId3), {url:tweet_url});
-                  chrome.tabs.update(parseInt(tabId4), {url:chrome.runtime.getURL("overview.html")+"?url="+message.url});
-
+                  if(tabId1==0 ||tabId2==0||tabId3==0||tabId4==0){
+                    var alexa_url="http://www.alexa.com/siteinfo/" + url;
+                    chrome.tabs.create({'url':alexa_url,'active':false},function(tab){
+                      tabId1=tab.id;
+                    });
+                    var whois_url="https://www.whois.com/whois/" + url;
+                    chrome.tabs.create({'url': whois_url,'active':false},function(tab){
+                      tabId2=tab.id;
+                    });
+                    var tweet_url="https://twitter.com/search?q="+open_url;
+                    chrome.tabs.create({'url': tweet_url,'active':false},function(tab){
+                      tabId3=tab.id;
+                    });
+                    chrome.tabs.create({url:chrome.runtime.getURL("overview.html")+"?url="+message.url,'active':false},function(tab){
+                      tabId4=tab.id;
+                    });
+                  }else{
+                    var alexa_url="http://www.alexa.com/siteinfo/" + url;
+                    chrome.tabs.update(parseInt(tabId1), {url:alexa_url});
+                    var whois_url="https://www.whois.com/whois/" + url;
+                    chrome.tabs.update(parseInt(tabId2), {url:whois_url});
+                    var tweet_url="https://twitter.com/search?q="+open_url;
+                    chrome.tabs.update(parseInt(tabId3), {url:tweet_url});
+                    chrome.tabs.update(parseInt(tabId4), {url:chrome.runtime.getURL("overview.html")+"?url="+message.url});  
+                  }
                 }else if(event.show_context=="window"){
+                  if(windowId1==0 ||windowId2==0||windowId3==0||windowId4==0){
+                    var alexa_url="http://www.alexa.com/siteinfo/" + url;
+                    chrome.windows.create({url:alexa_url, width:500, height:500, top:0, left:0, focused:false},function (win) {
+                      windowId1 = win.id;
+                    });
+                    var whois_url="https://www.whois.com/whois/" +url;
+                    chrome.windows.create({url:whois_url, width:500, height:500, top:500, left:0, focused:false},function (win) {
+                      windowId2 = win.id;
+                    });
+                    var tweet_url="https://twitter.com/search?q="+open_url;
+                    chrome.windows.create({url:tweet_url, width:500, height:500, top:0, left:500, focused:false},function (win) {
+                      windowId3 = win.id;
+                    });
+                    chrome.windows.create({url:chrome.runtime.getURL("overview.html")+"?url="+message.url,width:500, height:500, top:500, left:500, focused:false},function (win) {
+                      windowId4 = win.id;
+                    });
+                  }else{
                     chrome.tabs.query({
                       windowId: windowId1
                     }, function(tabs) {
@@ -582,7 +616,7 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
                       var tab=tabs[0];
                       chrome.tabs.update(tab.id, {url:chrome.runtime.getURL("overview.html")+"?url="+message.url});
                     });   
-                               
+                  }                               
                 }
               }
             });
