a01e972219615586dde02f3136d5f487c71ef6d2
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2018-06-25 10:23:28 +0530

    Changes added to settings and annotations file (Formatting style in html)

--- scripts/background.js
+++ scripts/background.js
@@ -240,7 +240,7 @@ function checkIt(wayback_url) {
 * License: AGPL-3
 * Copyright 2016, Internet Archive
 */
-var VERSION = "2.17.8";
+var VERSION = "2.17.9";
 Globalstatuscode="";
 var excluded_urls = [
   "localhost",
