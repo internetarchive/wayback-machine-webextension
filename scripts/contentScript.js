6fdaeee3dc36af64f5be9f59aa3efe1b5eae8962
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2018-08-16 20:14:32 +0530

    Changes done to only show the network part in Hoaxy Window

---
+++ scripts/contentScript.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    setTimeout(
+        function() {
+            console.log("here");
+            document.querySelector('.layout-button-graph').click();
+        },
+    100000);
+} 
