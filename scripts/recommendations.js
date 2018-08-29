3f34329cad27c1f3347386e112d08f612c876ea7
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2018-08-30 00:22:10 +0530

    Added dates in recommendation page

--- scripts/recommendations.js
+++ scripts/recommendations.js
@@ -11,21 +11,27 @@ function constructArticles(clips){
     let clip = entry['TV Clip'];
     let div = document.createElement('div');
     let img = document.createElement('img');
-    img.classList.add("resize_fit_center");
-    img.src = clip.preview_thumb;
     let anchor = document.createElement('a');
-    anchor.href = clip.preview_url;
-    anchor.appendChild(img);
     let p = document.createElement('p');
-    p.appendChild(anchor);
-    div.appendChild(p);
     let show = document.createElement('p');
     let strong = document.createElement('strong');
+    let showdate = document.createElement('p');
+
+    img.classList.add("resize_fit_center");
+    img.src = clip.preview_thumb;
+    anchor.href = clip.preview_url;
+
+    anchor.appendChild(img);
+    p.appendChild(anchor);
     strong.appendChild(document.createTextNode(clip.station + ": "));
     show.appendChild(strong);
     show.appendChild(document.createTextNode(clip.show));
     show.classList.add('resize_fit_center');
+    showdate.appendChild(document.createTextNode(
+      new Date(clip.date.slice(0,4) + "-" + clip.date.slice(4,6) + "-"+clip.date.slice(6,8)).toDateString()));
     div.appendChild(show);
+    div.appendChild(p);
+    div.appendChild(showdate);
     tray.appendChild(div);
   }
 }
