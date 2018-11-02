0caaa021f832a9e67354195a9f7979ade6f9f8e4
MaxReinisch <mreinisc@oberlin.edu>
2018-11-02 14:42:37 -0700

    Adds modal boxes to wikipedia pages

--- scripts/wikipedia.js
+++ scripts/wikipedia.js
@@ -1,6 +1,14 @@
 // This script adds buttons next to isbns on wikipedia pages that will redirect
 // the user to a readable digital copy of the referenced book.
 
+function addModal(){
+  $('body').append(
+    $('<div>').addClass('modal').prop('id', 'myModal').append(
+      $('<div>').addClass('modal-content')
+    ).click(closeModal)
+  )
+}
+
 // main method
 function addCitations () {
   getWikipediaBooks(location.href).then(data => {
@@ -8,8 +16,9 @@ function addCitations () {
     for (let book of books) {
       let isbn = getISBNFromCitation(book)
       let id = getIdentifier(data[isbn])
+      let metadata = getMetadata(data[isbn])
       if (id) {
-        let link = createLinkToArchive(id)
+        let link = createLinkToArchive(id, metadata)
         book.parentElement.append(link)
       }
     }
@@ -20,31 +29,43 @@ function addCitations () {
 // https://archive.org/services/context/books?url=...
 function getWikipediaBooks (url) {
   return fetch('https://archive.org/services/context/books?url=' + url)
-    .then(res => res.json())
-    .catch(err => console.log(err))
+  .then(res => res.json())
+  .catch(err => console.log(err))
 }
 
-function createLinkToArchive (id) {
+function createLinkToArchive (id, metadata) {
   let img = $('<img>')
-    .attr({ 'alt': 'Read', 'src': chrome.extension.getURL('images/icon.png') })[0]
+  .attr({ 'alt': 'Read', 'src': chrome.extension.getURL('images/icon.png') })[0]
   let a = $('<a>')
-    .attr({
-      'href': 'https://archive.org/details/' + id,
-      'class': 'btn-archive',
-      'style': 'padding: 5px;'
-    })
-    .prepend(img)
-    .hover(
-      function () {
-        $(this).text(' Read Now! ').prepend(img)
-      },
-      function () {
-        $(this).text('').prepend(img)
-      })[0]
-
-  return a
+  .attr({
+    'href': 'https://archive.org/details/' + id,
+    'class': 'btn-archive',
+    'style': 'padding: 5px;'
+  })
+  .prepend(img)
+  .hover(() => openModal(metadata))
+  return a[0]
 }
+function openModal(metadata){
 
+  $('.modal-content').empty().append(
+    $('<div>').addClass('modal-header').append(
+      $('<span>').addClass('close').text('x').click(closeModal),
+      $('<h2>').text(metadata.title)
+    ),
+    $('<div>').addClass('modal-body').append(
+      metadata.image ? $("<img>").attr({"class": "cover-img", "src": metadata.image}) : $("<p>").attr({"class": "cover-img"}).text("No cover available")
+    ),
+    $('<div>').addClass('modal-footer').append(
+      $('<a>').addClass(metadata.button_class).attr('href', metadata.link).text(metadata.button_text)
+    )
+  )
+  $('#myModal').show()
+}
+function closeModal(){
+  $('.modal-content').empty()
+  $('#myModal').hide()
+}
 function getIdentifier (book) {
   // identifier can be found as metadata.identifier or ocaid
   if (book) {
