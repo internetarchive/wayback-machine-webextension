b1942568d4ee6e0c1c574709eac1bb86825ded10
MaxReinisch <mreinisc@oberlin.edu>
2019-03-07 16:06:56 -0800

    added suggestions

--- scripts/about.js
+++ scripts/about.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-  const VERSION = chrome.runtime.getManifest().version;
+$(document).ready(function () {
+  const VERSION = chrome.runtime.getManifest().version
   const DATE = new Date().getFullYear()
-  $("#version").text("- "+VERSION)
-  $("#year").text(DATE)
-});
+  $('#version').text('- ' + VERSION)
+  $('#year').text(DATE)
+})
