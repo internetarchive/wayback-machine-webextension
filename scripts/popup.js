7b050ba515b2c911c82f33e41df9ecc37c5d221f
Chen Zhao <chenzhao@10-20-88-184.dynapool.wireless.nyu.edu>
2019-04-04 22:33:22 -0400

    removing testing code

--- scripts/popup.js
+++ scripts/popup.js
@@ -209,16 +209,13 @@ function borrow_books() {
           let stored_url = result.tab_url
           let detail_url = result.detail_url
           // Checking if the tab url is the same as the last stored one
-          console.log(stored_url, url, detail_url)
           if (stored_url === url) {
-            console.log('no request making in popup.js')
             // if so, then we can use the previously fetched url
             $('#borrow_books_tr').css({ 'display': 'block' }).click(function () {
               chrome.tabs.create({ url: detail_url })
             })
           } else {
             // if not, we can then fetch it again
-            console.log('making request in popup.js', url)
             get_amazonbooks(url).then(response => {
               if (response['metadata'] && response['metadata']['identifier-access']) {
                 let details_url = response['metadata']['identifier-access']
