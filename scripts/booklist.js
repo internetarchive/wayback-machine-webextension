0dcf36b540806301dc9aec65cb128f0ff9c1b681
Zerichen <cz1296@nyu.edu>
2019-04-09 11:50:04 -0400

    optimizing booklist.js

--- scripts/booklist.js
+++ scripts/booklist.js
@@ -3,17 +3,30 @@ function populateBooks(url) {
   getWikipediaBooks(url).then(data=>{
     $(".loader").hide();
     // No need to check the validation of data since promise is already being resolved
-    for(let isbn of Object.keys(data)) {  // Iterate over each book to get data
+    console.time()
+    for (let isbn of Object.keys(data)) {  // Iterate over each book to get data
       let metadata = getMetadata(data[isbn]);
-      if (metadata){
+      if (metadata) {
         let book_element = addBook(metadata);
-        if(metadata.readable){
-          $("#resultsTray").prepend(book_element);
+        const resultsTray = document.getElementById('resultsTray');
+        let containerForPrepend = document.createDocumentFragment();
+        const containerForAppend = document.createDocumentFragment();
+        if(metadata.readable) {
+          if (containerForPrepend.childNodes.length > 0) {
+            const firstChild = containerForPrepend.childNodes[0];
+            containerForPrepend.insertBefore(book_element, firstChild)
+          } else if (containerForPrepend.childNodes.length === 0) {
+            containerForPrepend.appendChild(book_element)
+          }
         } else {
-          $("#resultsTray").append(book_element);
+          containerForAppend.appendChild(book_element);
         }
+        const firstNode = resultsTray.childNodes[0];
+        resultsTray.insertBefore(containerForPrepend, firstNode);
+        resultsTray.appendChild(containerForAppend);
       }
     }
+    console.timeEnd()
   }).catch( function(error) {
     $(".loader").hide();
     $("#resultsTray").css("grid-template-columns", "none").append(
@@ -22,31 +35,56 @@ function populateBooks(url) {
   });
 }
 
-function addBook(metadata){
-  let text_elements = $("<div>").attr({"class": "text_elements"}).append(
-    $("<p>").append($("<strong>").text(metadata.title)),
-    $("<p>").text(metadata.author)
-  );
-  let details = $("<div>").attr({"class": "bottom_details"}).append(
-    metadata.image ? $("<img>").attr({"class": "cover-img", "src": metadata.image}) : $("<p>").attr({"class": "cover-img"}).text("No cover available"),
-    $("<button>").attr({"class": metadata.button_class}).text(metadata.button_text).click(function(){
-      chrome.storage.sync.get(['show_context'],function(event1){
-          if(event1.show_context==undefined){
-              event1.show_context="tab";
-          }
-          if(event1.show_context=="tab"){
-              chrome.tabs.create({url:metadata.link});
-          }else{
-            chrome.system.display.getInfo(function(displayInfo){
-              let height = displayInfo[0].bounds.height;
-              let width = displayInfo[0].bounds.width;
-              chrome.windows.create({url:metadata.link, width:width/2, height:height, top:0, left:0, focused:true});
-            });
-          }
-      });
-    })
-  );
-  return $("<div>").append(text_elements, details);
+function addBook(metadata) {
+  const text_elements = document.createElement('div');
+  text_elements.setAttribute('class', 'text_elements');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const strongTag = document.createElement('strong');
+  strongTag.textContent = metadata.title;
+  p1.appendChild(strongTag);
+  p2.textContent = metadata.author;
+  text_elements.appendChild(p1);
+  text_elements.appendChild(p2);
+
+  const details = document.createElement('div');
+  details.setAttribute('class', 'bottom_details');
+  if (metadata.image) {
+    const imgTag = document.createElement('img');
+    imgTag.setAttribute('class', 'cover-img');
+    imgTag.setAttribute('src', metadata.image);
+    details.appendChild(imgTag)
+  } else {
+    const pTag = document.createElement('p');
+    pTag.setAttribute('class', 'cover-img');
+    pTag.textContent = 'No cover available';
+    details.appendChild(pTag)
+  }
+
+  const btn = document.createElement('button');
+  btn.setAttribute('class', metadata.button_class);
+  btn.textContent = metadata.button_text;
+  btn.addEventListener('click', function() {
+    chrome.storage.sync.get(['show_context'], function(event1) {
+      if (event1.show_context==undefined) {
+        event1.show_context="tab";
+      }
+      if (event1.show_context=="tab") {
+        chrome.tabs.create({url:metadata.link});
+      } else {
+        chrome.system.display.getInfo(function(displayInfo) {
+          let height = displayInfo[0].bounds.height;
+          let width = displayInfo[0].bounds.width;
+          chrome.windows.create({url:metadata.link, width:width/2, height:height, top:0, left:0, focused:true});
+        });
+      }
+    });
+  })
+  details.appendChild(btn);
+  const divTag = document.createElement('div');
+  divTag.appendChild(text_elements);
+  divTag.appendChild(details);
+  return divTag;
 }
 
 if(typeof module !=="undefined") {
