34c5f233bfa790715eeb03def9af87d97673302f
Ritvik <ritvyk@gmail.com>
2019-04-28 21:21:25 +0530

    Voice Recognition JS File

---
+++ scripts/VoiceRec.js
@@ -0,0 +1,35 @@
+
+function startConverting()
+{
+    var reference= document.getElementById('search_input')
+    if('webkitSpeechRecognition' in window)
+    {
+        var recognition=new webkitSpeechRecognition();
+        recognition.continuous = false;
+        recognition.interimResults = true;
+        recognition.lang='[en-IN]';
+        recognition.start();
+        var finalTranscript='';
+        recognition.onresult=function(event)
+        {
+            var interimTrans='';
+            for(var i=event.resultIndex;i<event.results.length;i++)
+            {
+                var transcripts=event.results[i][0].transcript;
+                if(event.results[i].isFinal)
+                {
+                    finalTranscript+=transcripts;
+                    reference.value+='https://'+finalTranscript;
+                }
+                else{
+                    interimTrans+=transcripts;
+                }
+            }
+
+        };
+    }
+    else
+    {
+        alert(" Voice Recognition is not available \n browser please try Upgrading Chrome.");
+    }
+}
