00bb3cd82247b09997d598903c27192b99206991
Ritvik <ritvyk@gmail.com>
2019-05-01 09:59:31 +0530

    Update VoiceRec.js

--- scripts/VoiceRec.js
+++ scripts/VoiceRec.js
@@ -1,23 +1,20 @@
-
-function startConverting()
-{
+function startConverting(){
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+    var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+    var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
     var reference= document.getElementById('search_input')
-    if('webkitSpeechRecognition' in window)
-    {
+    if('webkitSpeechRecognition' in window){
         var recognition=new webkitSpeechRecognition();
         recognition.continuous = false;
         recognition.interimResults = true;
         recognition.lang='[en-IN]';
         recognition.start();
         var finalTranscript='';
-        recognition.onresult=function(event)
-        {
+        recognition.onresult=function( event ){
             var interimTrans='';
-            for(var i=event.resultIndex;i<event.results.length;i++)
-            {
+            for(var i=event.resultIndex;i<event.results.length;i++){
                 var transcripts=event.results[i][0].transcript;
-                if(event.results[i].isFinal)
-                {
+                if(event.results[i].isFinal){
                     finalTranscript+=transcripts;
                     reference.value+='https://'+finalTranscript;
                 }
@@ -28,8 +25,7 @@ function startConverting()
 
         };
     }
-    else
-    {
+    else{
         alert(" Voice Recognition is not available \n browser please try Upgrading Chrome.");
     }
 }
