bd6aae6389a0a45f0dbc315b9ed624c2316a285e
Zerichen <cz1296@nyu.edu>
2019-07-18 18:45:54 +0800

    create resource label setting

--- scripts/settings.js
+++ scripts/settings.js
@@ -7,28 +7,25 @@ addDocs()
 function initializeSettings () {
   chrome.storage.sync.get({
     show_context: 'tab',
-    auto_archive: false,
-    books: false,
+    citations: false,
+    resource: false,
     auto_update_context: false,
+    auto_archive: false,
     alexa: false,
     domaintools: false,
     tweets: false,
     wbmsummary: false,
     annotations: false,
     tagcloud: false,
-    doi: false,
-    citations: false,
-    news: false,
-    wikibooks: false,
-    showall: false,
-    resource: false
+    showall: false
   }, restoreOptions)
 }
 function restoreOptions (items) {
-  $('#auto-archive').prop('checked', items.auto_archive)
-  $('#books').prop('checked', items.books)
   $('#show_context').val(items.show_context)
+  $('#citations').prop('checked', items.citations)
+  $('#resource').prop('checked', items.resource)
   $('#auto-update-context').prop('checked', items.auto_update_context)
+  $('#auto-archive').prop('checked', items.auto_archive)
   $('#alexa').prop('checked', items.alexa)
   $('#domaintools').prop('checked', items.domaintools)
   $('#tweets').prop('checked', items.tweets)
@@ -36,31 +33,22 @@ function restoreOptions (items) {
   $('#annotations').prop('checked', items.annotations)
   $('#tagcloud').prop('checked', items.tagcloud)
   $('#showall').prop('checked', items.showall)
-  $('#news').prop('checked', items.news)
-  $('#wikibooks').prop('checked', items.wikibooks)
-  $('#doi').prop('checked', items.doi)
-  $('#citations').prop('checked', items.citations)
-  $('#resource').prop('checked', items.resource)
 }
 
 function save_options () {
   chrome.storage.sync.set({
     show_context: $('#show_context').val(),
-    auto_archive: $('#auto-archive').prop('checked'),
-    books: $('#books').prop('checked'),
+    citations: $('#citations').prop('checked'),
+    resource: $('#resource').prop('checked'),
     auto_update_context: $('#auto-update-context').prop('checked'),
+    auto_archive: $('#auto-archive').prop('checked'),
     alexa: $('#alexa').prop('checked'),
     domaintools: $('#domaintools').prop('checked'),
     tweets: $('#tweets').prop('checked'),
     wbmsummary: $('#wbmsummary').prop('checked'),
     annotations: $('#annotations').prop('checked'),
     tagcloud: $('#tagcloud').prop('checked'),
-    showall: $('#showall').prop('checked'),
-    news: $('#news').prop('checked'),
-    wikibooks: $('#wikibooks').prop('checked'),
-    doi: $('#doi').prop('checked'),
-    citations: $('#citations').prop('checked'),
-    resource: $('#resource').prop('checked')
+    showall: $('#showall').prop('checked')
   })
 }
 
@@ -83,13 +71,10 @@ function selectall () {
 function addDocs () {
   chrome.storage.sync.get(['newshosts'], function (items) {
     let docs = {
-      'auto-archive': 'Enables extension to identify URLs that have not previously been saved on the Wayback Machine.',
+      'citations': 'If a page contains a citation, enabling this will allow the extension to search the Archive for the citation and insert a link if found.',
+      'resource': 'Display "R" badge when user is viewing Amazon books/Wikipedia/Selected News Outlets and will show related resource in archive accordingly',
       'auto-update-context': 'Enabling this setting will update context windows when the page they are referencing changes.',
-      'books': 'Display a ‘B’ when viewing a book on Amazon that is found on The Archive.',
-      'wikibooks': 'Enables a button in the extension on wikipedia pages that will display a list of books cited on the page that are available on the archive.  Also adds popup buttons to the citations on the wikipedia page.',
-      'doi': 'Enables a button in the extension on wikipedia pages that will display a list of papers cited on the page that are available on the archive.',
-      'citations' : 'If a page contains a citation, enabling this will allow the extension to search the Archive for the citation and insert a link if found.',
-      'news': 'Enables a button in the extension on select news outlets that can recommend related tv news clips from the TV News Archive.  \n\n\n Works with articles posted on ' + items.newshosts.join(', '),
+      'auto-archive': 'Enables extension to identify URLs that have not previously been saved on the Wayback Machine.',
       'alexa': 'Displays what Alexa Internet knows about the site you are on (traffic data).',
       'domaintools': 'Displays what Domaintools.com Internet knows about the site you are on (domain registration).',
       'tweets': 'Show Tweets that include the URL you are on.',
