4082e6e30608dccf9d9ce42821034787bb3f577c
Max Reinisch <maxreinisch@Maxs-MacBook-Pro.local>
2019-07-31 14:45:35 -0700

    replaced DOM with Jquery

--- scripts/singleWindow.js
+++ scripts/singleWindow.js
@@ -178,28 +178,17 @@ function get_hypothesis() {
 }
 
 function openContextFeature(evt, feature) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+  $(".tabcontent").hide()
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
+  $(".tablinks").removeClass("active");
   // Show the current tab, and add an "active" class to the button that opened the tab
   $(feature).show();
   evt.currentTarget.className += " active";
 }
 
 function singlePageView(){
-  console.log("loading");
 
   const contexts_dic = {
     "alexa": get_alexa,
