b5aafe7a9867e65a845c53c9088dfa4f749f4df8
MaxReinisch <mreinisc@oberlin.edu>
2019-08-16 11:41:51 -0700

    Fixed bug introduced when removing url logging

--- scripts/background.js
+++ scripts/background.js
@@ -328,15 +328,15 @@ chrome.contextMenus.onClicked.addListener(function (click) {
       let wayback_url;
       let wmIsAvailable = true;
       if (click.menuItemId === 'first') {
-        wayback_url = 'https://archive.org/web/0/' + encodeURI(page_url);
+        wayback_url = 'https://web.archive.org/web/0/' + encodeURI(page_url);
       } else if (click.menuItemId === 'recent') {
-        wayback_url = 'https://archive.org/web/2/' + encodeURI(page_url);
+        wayback_url = 'https://web.archive.org/web/2/' + encodeURI(page_url);
       } else if (click.menuItemId === 'save') {
         wmIsAvailable = false;
         wayback_url = 'https://archive.org/save/' + encodeURI(page_url);
       } else if (click.menuItemId === 'all') {
         wmIsAvailable = false;
-        wayback_url = 'https://archive.org/web/*/' + encodeURI(page_url);
+        wayback_url = 'https://web.archive.org/web/*/' + encodeURI(page_url);
       }
       URLopener(wayback_url, page_url, wmIsAvailable);
     }
