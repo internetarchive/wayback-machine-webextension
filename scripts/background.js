b4a2d3c75ec79b79a8780b555ada832097722c44
tikhsuP <pushkit20@gmail.com>
2020-05-25 19:24:22 +0530

    APNEWS doesn't matches www

--- scripts/background.js
+++ scripts/background.js
@@ -16,7 +16,7 @@ let toolbarIconState = {}
 let tabIdPromise
 var WB_API_URL = 'https://archive.org/wayback/available'
 var newshosts = [
-  'www.apnews.com',
+  'apnews.com',
   'www.factcheck.org',
   'www.forbes.com',
   'www.huffpost.com',
