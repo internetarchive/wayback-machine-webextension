3cf706d87ec420af7955954b14d88d925caed00c
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2020-06-23 23:42:14 +0530

    showing resources list

--- scripts/resource_list.js
+++ scripts/resource_list.js
@@ -6,17 +6,35 @@ window.onload = function () {
   }
 
   async function show_resource_data(job_id,url_name) {
-    let vdata
+    let vdata = {}
     let status = 'start'
+    let resource_list_data = new Set()
+    let old_resource_length = 0;
     const val_data = new URLSearchParams()
     val_data.append('job_id', job_id)
     var current_url = document.getElementById('current_url')
     current_url.innerHTML = url_name + '....'
     while ((status === 'start') || (status === 'pending')) {
-      var dom_status = document.getElementById('current_status');
+      var dom_status = document.getElementById('current_status')
       dom_status.innerHTML = 'Pending'
       // update UI
-
+      var new_resource_length
+      if ('resources' in vdata){
+        new_resource_length = vdata.resources.length
+        vdata.resources.forEach( (element) => {
+          resource_list_data.add(element)
+        });
+      }
+      if(new_resource_length>old_resource_length){
+        old_resource_length = new_resource_length
+        console.log("hey")
+        for (let item of resource_list_data){
+          $('#resource_list').append(
+            $('<li>').append(item)
+          );   
+        }
+      }
+      console.log(resource_list_data)
       await sleep(1000)
       const timeoutPromise = new Promise(function (resolve, reject) {
         setTimeout(() => {
@@ -45,6 +63,18 @@ window.onload = function () {
   
     if (vdata.status === 'success') {
       dom_status.innerHTML = 'Success'
+      new_resource_length = vdata.resources.length
+      vdata.resources.forEach( (element) => {
+        resource_list_data.add(element)
+      });
+      if(new_resource_length>old_resource_length){
+        old_resource_length = new_resource_length
+        for (let item of resource_list_data){
+          $('#resource_list').append(
+            $('<li>').append(item)
+          );   
+        }
+      }
       // update UI
     } else if (!vdata.status || (status === 'error')) {
       dom_status.innerHTML = 'Failed'
