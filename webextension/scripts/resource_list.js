d2c7f3a8b04b7ccfed265da3f0abc98053e12136
tikhsuP <pushkit20@gmail.com>
2020-06-30 22:20:33 +0530

    Add Style

--- webextension/scripts/resource_list.js
+++ webextension/scripts/resource_list.js
@@ -1,53 +1,51 @@
-window.onload = function () {
-    var url = new URL(window.location.href)
-    var url_name = url.searchParams.get('url')
-    show_resource_data(url_name)
-  }
+window.onload = () => {
+  var url = new URL(window.location.href)
+  var url_name = url.searchParams.get('url')
+  show_resource_data(url_name)
+}
 
 function show_resource_data(url_name) {
   let vdata = {}
   let status = 'start'
   let resource_list_data = new Set()
-  let old_resource_length = 0;
-  $('#current-url').text(url_name+'.....')
+  let old_resource_length = 0
+  $('#current-url').text(url_name + '.....')
   chrome.runtime.onMessage.addListener(
-    function(message) {
-      $(".error").hide()
-      $(".row").show()
-      if (message.message == 'resource_list_show'){
+    (message) => {
+      $('.error').hide()
+      if (message.message === 'resource_list_show') {
         vdata = message.data
         status = message.data.status
-        $("#current-status").text(status.charAt(0).toUpperCase()+status.slice(1))
-
-        if(status === 'pending' || status === 'start'){
-          var new_resource_length
-          if ('resources' in vdata){
+        $('#current-status').text(status.charAt(0).toUpperCase() + status.slice(1))
+        if (status === 'pending' || status === 'start') {
+          let new_resource_length
+          if ('resources' in vdata) {
             new_resource_length = vdata.resources.length
-            vdata.resources.forEach( (element) => {
+            vdata.resources.forEach((element) => {
               resource_list_data.add(element)
-            });
+            })
           }
-          if(new_resource_length>old_resource_length){
+          if (new_resource_length > old_resource_length) {
             old_resource_length = new_resource_length
-            for (let item of resource_list_data){
+            for (let item of resource_list_data) {
               $('#resource-list-container').append(
                 $('<p>').append(item)
-              );   
+              )
             }
           }
-        }else if (status === 'success'){
-          $(".text-right").show();
+        } else if (status === 'success') {
+          $('.text-right').show()
           new_resource_length = vdata.resources.length
-          $("#spn-elements-counter").text(new_resource_length)
-          vdata.resources.forEach( (element) => {
+          $('#spn-elements-counter').text(new_resource_length)
+          vdata.resources.forEach((element) => {
             resource_list_data.add(element)
-          });
-          if(new_resource_length>old_resource_length){
+          })
+          if (new_resource_length > old_resource_length) {
             old_resource_length = new_resource_length
-            for (let item of resource_list_data){
+            for (let item of resource_list_data) {
               $('#resource-list-container').append(
                 $('<p>').append(item)
-              );   
+              )
             }
           }
         } else if (!status || (status === 'error')) {
@@ -57,4 +55,3 @@ function show_resource_data(url_name) {
     }
   )
 }
-  
