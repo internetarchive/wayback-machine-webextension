fa96a8ac5ba3b7904712016e224998a9e42df163
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2020-07-02 01:45:31 +0530

    remove repetition of appending to container

--- webextension/scripts/resource_list.js
+++ webextension/scripts/resource_list.js
@@ -8,7 +8,6 @@ function show_resource_data(url_name) {
   let vdata = {}
   let status = 'start'
   let resource_list_data = new Set()
-  let old_resource_length = 0
   let new_resource_length
   $('#current-url').text(url_name + '.....')
   chrome.runtime.onMessage.addListener(
@@ -18,36 +17,20 @@ function show_resource_data(url_name) {
         vdata = message.data
         status = message.data.status
         $('#current-status').text(status.charAt(0).toUpperCase() + status.slice(1))
-        if (status === 'pending' || status === 'start') {
-          if ('resources' in vdata) {
-            new_resource_length = vdata.resources.length
-            vdata.resources.forEach((element) => {
-              resource_list_data.add(element)
-            })
-          }
-          if (new_resource_length > old_resource_length) {
-            old_resource_length = new_resource_length
-            for (let item of resource_list_data) {
-              $('#resource-list-container').append(
-                $('<p>').append(item)
-              )
-            }
-          }
-        } else if (status === 'success') {
-          $('.text-right').show()
-          new_resource_length = vdata.resources.length
-          $('#spn-elements-counter').text(new_resource_length)
+        if ('resources' in vdata) {
           vdata.resources.forEach((element) => {
             resource_list_data.add(element)
           })
-          if (new_resource_length > old_resource_length) {
-            old_resource_length = new_resource_length
-            for (let item of resource_list_data) {
-              $('#resource-list-container').append(
-                $('<p>').append(item)
-              )
-            }
-          }
+        }
+        for (let item of resource_list_data) {
+          $('#resource-list-container').append(
+            $('<p>').append(item)
+          )
+        }
+        if (status === 'success') {
+          $('.text-right').show()
+          new_resource_length = vdata.resources.length
+          $('#spn-elements-counter').text(new_resource_length)
         } else if (!status || (status === 'error')) {
           $('#resource-list-container').hide()
         }
