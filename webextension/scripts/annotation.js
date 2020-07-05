3d8e9c19afc4644d8aa463ae4bb315f6a75d0e9b
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2020-07-05 19:47:14 +0530

    chnange link to button in annotation context screen

--- webextension/scripts/annotation.js
+++ webextension/scripts/annotation.js
@@ -47,9 +47,9 @@ function get_annotations (type = 'url') {
         item.find('#text-contain').html(exactData)
         item.find('.title').html(title)
         item.find('.links').append(
-          $('<a>').attr({ 'href': rowData.links.incontext, 'id': 'link-incontext', 'target': '_blank' })
+          $('<a>').attr({ 'href': rowData.links.incontext, 'id': 'link-incontext', 'class': 'button', 'target': '_blank' })
             .html('Click to see in context'),
-          $('<a>').attr({ 'href': rowData.links.html, 'id': 'link-html', 'target': '_blank' })
+          $('<a>').attr({ 'href': rowData.links.html, 'id': 'link-html',  'class': 'button', 'target': '_blank' })
             .html('Click to see the HTML')
         )
         if (rowData.target[0].hasOwnProperty('selector')) {
