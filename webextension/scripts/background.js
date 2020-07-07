a4fa6f5f2e259df0677ca609e8a012b3c47ec07c
Carl Gorringe <git@gorringe.org>
2020-07-06 19:20:43 -0700

    notify error on SPN; update status API URL

--- webextension/scripts/background.js
+++ webextension/scripts/background.js
@@ -71,7 +71,16 @@ function savePageNow(tabId, page_url, silent = false, options = []) {
         if (!silent) {
           notify('Saving ' + page_url)
         }
-        validate_spn(tabId, res.job_id, silent)
+        if (('job_id' in res) && (res.job_id !== 'undefined')) {
+          validate_spn(tabId, res.job_id, silent)
+        } else {
+          // handle error
+          let msg = res.message || 'Please Try Again'
+          chrome.runtime.sendMessage({ message: 'save_error', error: msg })
+          if (!silent) {
+            notify('Error: ' + msg)
+          }
+        }
       })
   }
 }
@@ -117,7 +126,7 @@ async function validate_spn(tabId, job_id, silent = false) {
         reject(new Error('timeout'))
       }, 30000)
       if ((status === 'start') || (status === 'pending')) {
-        fetch('https://web.archive.org/save/status', {
+        fetch(hostURL + 'save/status', {
           credentials: 'include',
           method: 'POST',
           body: val_data,
