824640b6575f3a5fdb865f472e374b2c912b29ca
Carl Gorringe <git@gorringe.org>
2020-07-08 00:27:39 -0700

    Xcode generated code

---
+++ safari/Wayback Machine/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Wayback Machine
+//
+//  Created by Carl on 7/8/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
