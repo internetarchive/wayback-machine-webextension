4c7cc1ed1f4f1c77d8a845f231324e839e3b1591
tikhsuP <pushkit20@gmail.com>
2020-07-24 14:21:20 +0530

    Tweak Styles

--- webextension/scripts/fact-check.js
+++ webextension/scripts/fact-check.js
@@ -9,14 +9,16 @@ if (isValidUrl(url) && isNotExcludedUrl(url)) {
   chrome.runtime.sendMessage({ message: 'getFactCheckResults', url: url }, (resp) => {
     if (resp && resp.results) {
       $('.loader').hide()
-      let row = $('<div class="fact-checks-list flex-container">')
       let item = resp.results.meta
       for (i = 0; i < item.indicators.length; i++) {
+        let row = $('<div class="fact-checks-list flex-container">')
         let checkedBy = $('<div class="name">').text(item.indicators[i].name)
-        let relatedArticle = $('<a target="_blank">').text(item.indicators[i].url).attr('href', item.indicators[i].url)
-        let confidenceScore = $('<div class="url-item">').text(item.indicators[i].confidence)
+        let relatedAnalysis = $('<a target="_blank">').text(item.indicators[i].url).attr('href', item.indicators[i].url)
+        let score = $('<span class="highlight">').text(item.indicators[i].confidence)
+        let confidenceScore = $('<div>').text('Confidence Score: ').append(score)
+
         $('#results-container').append(
-          row.append(checkedBy, relatedArticle, confidenceScore)
+          row.append(checkedBy, relatedAnalysis, confidenceScore)
         )
       }
     }
