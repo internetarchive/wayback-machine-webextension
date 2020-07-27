1d7b221dbf75856f1116dbc9b9e7805a5e0d71e3
tikhsuP <pushkit20@gmail.com>
2020-07-27 14:37:27 +0530

    Add label based on confidence score

--- webextension/scripts/fact-check.js
+++ webextension/scripts/fact-check.js
@@ -3,6 +3,24 @@
 // from 'utils.js'
 /*   global getUrlByParameter, isNotExcludedUrl, isValidUrl */
 
+function cScore(score) {
+  let value = ''
+  const confidenceScoreDict = {
+    high: 'High',
+    medium: 'Medium',
+    low: 'Low',
+  }
+
+  if (score > 0.7) {
+    value = 'high'
+  } else if (score > 0.5) {
+    value = 'medium'
+  } else if (score >= 0) {
+    value = 'low'
+  }
+  return confidenceScoreDict[value]
+}
+
 const url = getUrlByParameter('url')
 $('#fact-check-url').text(url)
 if (isValidUrl(url) && isNotExcludedUrl(url)) {
@@ -14,8 +32,10 @@ if (isValidUrl(url) && isNotExcludedUrl(url)) {
         let row = $('<div class="fact-checks-list flex-container">')
         let checkedBy = $('<div class="name">').text(item.indicators[i].name)
         let relatedAnalysis = $('<a target="_blank">').text(item.indicators[i].url).attr('href', item.indicators[i].url)
-        let score = $('<span class="highlight">').text(item.indicators[i].confidence)
-        let confidenceScore = $('<div>').text('Confidence Score: ').append(score)
+        let scoreValue = $('<span class="highlight">').text((Math.round((item.indicators[i].confidence)*100)))
+        let sRating = cScore(item.indicators[i].confidence)
+        let scoreLabel = $(`<span class="score-label ${sRating}">`).text(sRating)
+        let confidenceScore = $('<div>').text('Confidence Score: ').append(scoreValue, scoreLabel)
 
         $('#results-container').append(
           row.append(checkedBy, relatedAnalysis, confidenceScore)
