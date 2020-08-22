f9cc504248ffd322c1321dbb8b3eb8afd8f17e28
Carl Gorringe <git@gorringe.org>
2020-08-21 19:35:16 -0700

    other tests still broken

--- webextension/scripts/utils.js
+++ webextension/scripts/utils.js
@@ -40,6 +40,7 @@ let isObject = (a) => (!!a) && (a.constructor === Object)
 let searchValue
 let private_before_state
 
+// TODO FIXME: This breaks when running tests!
 chrome.storage.local.get(['private_before_state'], (event) => {
   private_before_state = new Set(event.private_before_state)
 })
