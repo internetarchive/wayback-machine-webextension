ee658501ae3637d3626ab80d05db535ff557597b
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2021-01-05 17:05:47 +0530

    use gBrowser instead of getBrowser()

--- webextension/scripts/background.js
+++ webextension/scripts/background.js
@@ -5,7 +5,7 @@
 
 // from 'utils.js'
 /*   global isNotExcludedUrl, get_clean_url, isArchiveUrl, isValidUrl, notify, openByWindowSetting, sleep, wmAvailabilityCheck, hostURL, isFirefox */
-/*   global initDefaultOptions, afterAcceptOptions, badgeCountText, getWaybackCount, newshosts, dateToTimestamp, getBrowser */
+/*   global initDefaultOptions, afterAcceptOptions, badgeCountText, getWaybackCount, newshosts, dateToTimestamp, gBrowser */
 
 var manifest = chrome.runtime.getManifest()
 // Load version from Manifest.json file
@@ -32,7 +32,7 @@ var private_before_default = new Set([
 function rewriteUserAgentHeader(e) {
   for (var header of e.requestHeaders) {
     if (header.name.toLowerCase() === 'user-agent') {
-      header.value = header.value + ' Wayback_Machine_'+ getBrowser().charAt(0).toUpperCase() + getBrowser().slice(1) + '/' + VERSION + ' Status-code/' + globalStatusCode
+      header.value = header.value + ' Wayback_Machine_'+ gBrowser.charAt(0).toUpperCase() + gBrowser.slice(1) + '/' + VERSION + ' Status-code/' + globalStatusCode
     }
   }
   return { requestHeaders: e.requestHeaders }
