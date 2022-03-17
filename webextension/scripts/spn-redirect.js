23e462d6d48b5e10742df38efcf155266b998417
Carl Gorringe <git@gorringe.org>
2022-03-17 01:06:15 -0700

    trying to fix spn-redirect.js but not working

--- webextension/scripts/spn-redirect.js
+++ webextension/scripts/spn-redirect.js
@@ -1,7 +1,18 @@
 // spn-redirect.js
+console.log('spn-redirect.js start') // DEBUG
 
 // submits a POST form to open SPN Wayback Machine website with url prefilled.
 let params = (new URL(window.location)).searchParams
+console.log(params) // DEBUG
 let url = params.get('url')
+console.log(url) // DEBUG
 document.getElementById('url-preload').value = url
-document.getElementById('save-form').submit()
+// document.getElementById('save-form').submit()
+console.log('waiting') // DEBUG
+
+// TEST
+setTimeout(() => {
+  console.log('submit form') // DEBUG
+  // BUG in Safari: form is not POSTing url_preload! (neither private nor normal modes)
+  document.getElementById('save-form').submit()
+}, 20000)
