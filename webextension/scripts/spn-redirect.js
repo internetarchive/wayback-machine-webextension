f73e0a4f2c37c82c76aa512dfa0d5df63c1ffb59
Carl Gorringe <git@gorringe.org>
2022-03-22 22:42:29 -0700

    remove spn-redirect

--- webextension/scripts/spn-redirect.js
+++
@@ -1,7 +0,0 @@
-// spn-redirect.js
-
-// submits a POST form to open SPN Wayback Machine website with url prefilled.
-let params = (new URL(window.location)).searchParams
-let url = params.get('url')
-document.getElementById('url-preload').value = url
-document.getElementById('save-form').submit()
