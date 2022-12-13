14cf9fb339cb1010b123f07f6a1c2a983f832091
Anish Sarangi <anish@archive.org>
2022-12-13 15:43:31 +0530

    remove mastodon.archive.org from excluded urls

--- webextension/scripts/utils.js
+++ webextension/scripts/utils.js
@@ -391,7 +391,7 @@ function isArchiveUrl(url) {
   if (typeof url !== 'string') { return false }
   try {
     const hostname = new URL(url).hostname
-    return (hostname === 'archive.org') || hostname.endsWith('.archive.org')
+    return (!hostname.startsWith('mastodon')) && (hostname === 'archive.org' || hostname.endsWith('.archive.org'))
   } catch (e) {
     // url not formated correctly
     return false
